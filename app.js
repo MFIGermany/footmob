9ac970209c7d1dd1cb712aae0b2805370ee0b218
Yoenry <yvanega@gmail.com>
2024-05-08 12:24:49 -0300

    especificando la rel canonical

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config({ path: './.env' })
 export const createApp = () => {
   const app = express()
   app.use(json())
-  app.use(cors({ origin: '*' }))
+  app.use(cors())
   app.disable('x-powered-by')
 
   app.use(express.static(path.join(__dirname, './static/public')))
