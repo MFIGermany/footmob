cd1a80e69b7a6184df595332e6656cc6fcd47a4e
Yoenry <yvanega@gmail.com>
2024-11-08 10:04:57 -0200

    uso de vpn

--- controllers/footmob.js
+++ controllers/footmob.js
@@ -259,7 +259,7 @@ export class FootMobController {
       const response = await this.footMob.getMatches(base_url + 'home.php')
       
       if(response){
-        console.log('entre')
+        //console.log('entre')
         // Obtener todos los elementos li dentro del ul
         const menuItems = response.querySelectorAll('ul.menu > li')
         
